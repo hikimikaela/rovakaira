2564ca193c01961c7f8cfe2266c90d53017beed7
hikimikaela <hallemi@boniquiashareef.laatukolmio.fi>
2017-04-21 09:27:28 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// Webbiäpin käyttöliittymä
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return <div>Koko setti</div>;
+
+}
+
+ReactDOM.render (<App />, document.querySelector('.container'));
